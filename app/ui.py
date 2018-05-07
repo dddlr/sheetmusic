88aeb6da9cffa702fc0dd35b71105724a2560d6e
ddlr <epic.emolga@gmail.com>
2018-05-08 09:53:45 +1200

    Add instrument info to music

--- app/ui.py
+++ app/ui.py
@@ -1,16 +1,19 @@
 from app import app
 from flask import render_template, redirect, request, url_for
-from app.models import Music
+from app.models import Music, Instrument
 
 @app.route('/')
 def index():
     '''Browse all sheet music.'''
     music = Music.query.all()
-    return render_template('test.html', music=music)
+    return render_template('music.html', music=music, title="Home")
 
 def sheet_template(sheet):
     '''The actual template rendering for sheet music.'''
-    return render_template('test.html', music=[sheet])
+    page_title = "Viewing {}".format(sheet.name)
+    # Super hacky but it works (TODO)
+    instruments = list(map(lambda i: Instrument.query.get(i.instrument_id), sheet.instruments))
+    return render_template('sheet.html', sheet=sheet, title=page_title, instruments=instruments)
 
 @app.route('/sheet/<int:id>/<string:url>/')
 def sheet_with_name(id, url):
