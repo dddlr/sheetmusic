d567207d0ed57d558f681e35a8c1cbbbe002fafb
ddlr <ddlr@users.noreply.github.com>
2018-06-18 09:20:18 +1200

    Finish admin_style, admin_instrument

--- app/admin.py
+++ app/admin.py
@@ -1,61 +1,77 @@
 from flask import Blueprint, render_template, request
+from sqlalchemy import exc
 from app import db
 from app.models import Instrument, Style
-from app.validate import check_instrument, check_style
+from app.validate import check_instrument, check_style, RowExistsError
 
 bp = Blueprint('admin', __name__)
 
 @bp.route('/admin/style/', methods=['GET', 'POST'])
 def add_style():
+    """Renders the page for adding a style/genre, as well as handling the
+    addition of a style into the database."""
+    error = False
+    status = None
     if request.method == 'POST':
-        error = False
-        status = None
         try:
             name = request.form['style']
             description = request.form['description']
-            print('eee')
-            if check_style(name, description):
-                print('aaa')
-                style = Style(style=name, description=description)
-                # TODO: figure out why no error is being returned when name
-                # equals null
-                db.session.add(style)
-                db.session.commit()
-                print('bbb')
-                status = "Added successfully."
-            else:
-                # should never happen
-                error = True
-                status = "Something went wrong. Please try again."
-        except:
+            check_style(name, description)
+            style = Style(style=name, description=description)
+            db.session.add(style)
+            db.session.commit()
+        except exc.IntegrityError as err:
             error = True
-            status = "something went wrong"
-        finally:
-            print('status is', status)
-            return render_template('admin_style.html', title="Admin", status=status, error=error)
-    return render_template('admin_style.html', title="Admin")
+            print('/admin/style/:', err)
+            status = "Something went wrong. Please try again."
+        except ValueError as err:
+            # Covers ValueError and RowExistsError
+            error = True
+            status = err
+        except Exception as err:
+            error = True
+            print("Unexpected error:", err)
+            status = "Something went wrong. Please try again."
+        else:
+            status = "Added successfully."
+    return render_template('admin_style.html', title="Admin", status=status, error=error)
 
 @bp.route('/admin/instrument/', methods=['GET', 'POST'])
 def add_instrument():
+    """Renders the page for adding an instrument, as well as handling the
+    addition of an instrument into the database."""
+    error = False
+    status = None
     if request.method == 'POST':
-        error = False
-        status = None
         try:
+            print('qqqq')
             name = request.form['name']
             info = request.form['info']
             range = request.form['range']
             image = request.form['image']
-            if check_instrument(name, info, range, image):
-                instrument = Instrument(name=name, info=info, range=range, image=image)
-                print(instrument)
-                db.session.add(instrument)
-                db.session.commit()
-        except InputError:
+            check_instrument(name, info, range, image)
+            print('eeee')
+            instrument = Instrument(name=name, info=info, range=range, image=image)
+            print('aaaa')
+            db.session.add(instrument)
+            db.session.commit()
+        except exc.IntegrityError as err:
             error = True
-            status = 'something went wrong'
+            print('/admin/instrument/:', err)
+            status = "Something went wrong. Please try again."
+        except ValueError as err:
+            error = True
+            status = err
+        except Exception as err:
+            error = True
+            print("Unexpected error:", err)
+            status = "Something went wrong. Please try again."
+        else:
+            status = "Added successfully."
         finally:
-            return render_template('admin_instrument.html', title="Admin", status=status, error=error)
-    return render_template('admin_instrument.html', title="Admin")
+            print(status)
+            print(error)
+    return render_template('admin_instrument.html', title="Admin", status=status, error=error)
 
 @bp.route('/admin/music/', methods=['GET', 'POST'])
 def add_music():
--- app/validate.py
+++ app/validate.py
@@ -1,10 +1,44 @@
+from app.models import Instrument, Style
+from sqlalchemy import exc
+
+class RowExistsError(ValueError):
+    """Exception raised when a row in the database already exists."""
+
+    def __init__(self, message):
+        self.message = message
+
 def check_style(style, description=None):
-    # TODO
-    # Check null values
-    # Query database for if style exists
+    """Checks if style is valid according to the database models."""
+    if Style.query.get(style):
+        raise RowExistsError("This style already exists.")
+    elif len(style) > 32 or len(description) > 200:
+        raise ValueError("Style or description can be a maximum of 32 or 200 \
+            characters only.")
+    elif style == 'bbb':
+        raise ValueError("This is a value error.")
+    elif not style:
+        raise ValueError("Name of style cannot be empty.")
     return True
 
 def check_instrument(name, info=None, range=None, image=None):
+    """Checks if style is valid according to the database models."""
+    too_long = []
+    if Instrument.query.filter_by(name=name).scalar() is not None:
+        raise RowExistsError("Instrument already exists.")
+    if len(name) > 32:
+        too_long.append('name')
+    if len(info) > 200:
+        too_long.append('info')
+    if len(range) > 100:
+        too_long.append('range')
+    if len(image) > 100:
+        too_long.append('image URL')
+    if len(too_long) > 0:
+        raise ValueError("The following instrument information is too long: " +
+            ', '.join(too_long))
+
+    elif not name:
+        raise ValueError("Name of instrument cannot be empty.")
     return True
 
 def check_music():
