63c79dac1386aa768c21f55826916c760bd0cafe
ddlr <ddlr@users.noreply.github.com>
2018-08-03 09:24:36 +1200

    Work on test framework

--- test_models.py
+++ test_models.py
@@ -3,24 +3,28 @@ import tempfile
 import pytest
 
 from app import create_app, db
-from app.models import Instrument, Style, OriginalAuthor, Music, MusicInstrument
+from app.models import Instrument, Style, OriginalAuthor, Music
 
 from config import Config
 
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + tempfile.mkstemp()[1]
     TESTING = True
+    WTF_CSRF_ENABLED = False
 
 @pytest.fixture
 def client():
     # set custom config via environment variables
     app = create_app(TestConfig)
     client = app.test_client()
+    ctx = app.app_context()
 
-    print('client')
+    # inspired by
+    # http://www.patricksoftwareblog.com/testing-a-flask-application-using-pytest/
 
-    # TODO: fix this function to use Blueprints
+    ctx.push()
     yield client
+    ctx.pop()
 
-def test(client):
-    print(Instrument.query.all())
+def test_something(client):
+    print('hello world')
